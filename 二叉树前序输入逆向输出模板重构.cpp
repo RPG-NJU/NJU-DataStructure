489810f2251f138ce48957f524c7f1b8c1b0ac85
RPG <41836383+HELLORPG@users.noreply.github.com>
2018-11-01 00:12:48 +0800

    第六次作业

---
+++ 二叉树前序输入逆向输出模板重构.cpp
@@ -0,0 +1,506 @@
+//#pragma once
+#ifndef GRP_BINTREE
+#define GRP_BINTREE
+
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+template <typename T>
+class GRPStack;
+
+//template <typename T>
+//class GRPNode
+//{
+//	friend class GRPStack<T>;
+//protected:
+//	T data;
+//	GRPNode *before;
+//	GRPNode *next;
+//	GRPNode() = default;
+//	GRPNode(T data, GRPNode *before) :data(data), before(before), next(nullptr) {}
+//	~GRPNode() {}
+//};
+
+
+#ifndef GRP_VECTOR
+#define GRP_VECTOR
+#define DEFAULTSIZE 5
+
+template <typename T>
+class GRPVector
+{
+protected:
+	T *data;
+	size_t max_size;
+	size_t real_size;
+public:
+	GRPVector();
+
+	void push_back(T input);
+	void pop_back();
+	size_t size() const;
+
+	T& front();
+	T& back();
+	T& operator[](int i);
+
+	int find(T which) const;//寻找某元素，返回它的索引
+};
+
+template <typename T>
+GRPVector<T>::GRPVector() : data(new T[DEFAULTSIZE]), max_size(5), real_size(0) {}
+
+
+template <typename T>
+size_t GRPVector<T>::size() const
+{
+	return real_size;
+}
+
+template <typename T>
+void GRPVector<T>::push_back(T input)
+{
+	if (real_size == max_size)
+	{
+		T *old = data;
+		data = new T[2 * max_size];//开一个扩容一倍的数组
+		for (size_t i(0); i < max_size; ++i)
+		{
+			data[i] = old[i];
+		}
+		max_size *= 2;//将扩容反馈
+	}
+	//无论需不需要扩容，后续的过程是一样的
+	data[real_size] = input;
+	++real_size;
+	return;
+}
+
+template <typename T>
+void GRPVector<T>::pop_back()
+{
+	//现如今没有打算完成可以自动减少长度的功能
+	--real_size;//直接将最后的标识向前即可，这样子就可以避免访问
+	return;
+}
+
+template <typename T>
+T& GRPVector<T>::operator[](int i)
+{
+	//cout << -static_cast<int>(real_size) << endl;
+	//cout << (i >= -static_cast<int>(real_size)) << endl;
+	if ((i < static_cast<int>(real_size)) && (i >= -static_cast<int>(real_size)))
+	{
+		if (i >= 0)
+			return data[i];
+		else
+			return data[this->size() + i];
+	}
+
+	else
+	{
+		cout << "This GRPVector's size is " << real_size << endl;
+		cout << "You should not read the place [" << i << "]" << endl;
+		exit(-1);
+	}
+}
+
+template <typename T>
+T& GRPVector<T>::front()
+{
+	return (*this)[0];
+}
+
+template <typename T>
+T& GRPVector<T>::back()
+{
+	return (*this)[this->size()];
+}
+
+template <typename T>
+int GRPVector<T>::find(T which) const
+{
+	for (int i(0); i < real_size; ++i)
+	{
+		if (data[i] == which)
+			return i;
+	}
+	return -1;//作为无此元素的标志
+}
+
+#endif
+
+template <typename T>
+class GRPSinNode
+{
+	friend class GRPStack<T>;
+protected:
+	T data;
+	GRPSinNode *next;
+	GRPSinNode() :data(*(new T)), next(nullptr) {}
+	GRPSinNode(T data) :data(data), next(nullptr) {}
+	GRPSinNode(T data, GRPSinNode *next) :data(data), next(next) {}
+	~GRPSinNode() {}
+};
+
+template <typename T>
+class GRPStack
+{
+private:
+	GRPSinNode<T> *FIRST/*, *current*/;
+	size_t stack_size;
+public:
+	GRPStack();
+	~GRPStack();
+	void push(T input);
+	T pop();
+	T& top();
+	size_t size() const;
+	void show() const;
+	bool empty() const;
+	void clear();
+};
+
+//template <typename T>
+//GRPStack<T>::GRPStack()
+//{
+//	FIRST = current = new GRPNode<T>;
+//	stack_size = 0;
+//}
+//
+//template <typename T>
+//GRPStack<T>::~GRPStack()
+//{
+//	GRPNode<T> *kill(FIRST);
+//	for (; FIRST != nullptr;)
+//	{
+//		kill = FIRST;
+//		FIRST = FIRST->next;
+//		delete kill;
+//	}
+//}
+//
+//template <typename T>
+//void GRPStack<T>::push(T input)
+//{
+//	GRPNode<T> *add = new GRPNode<T>(input, current);
+//	current->next = add;
+//	current = add;
+//	++stack_size;
+//	return;
+//}
+//
+//template <typename T>
+//T GRPStack<T>::pop()
+//{
+//	GRPNode<T> *p = current;
+//	current = current->before;
+//	current->next = nullptr;
+//	--stack_size;
+//	T pop_ret(p->data);
+//	delete p;//换成delete[]就会不行，待解
+//	return pop_ret;
+//}
+//
+//template <typename T>
+//T GRPStack<T>::top()
+//{
+//	return current->data;
+//}
+//
+//template <typename T>
+//size_t GRPStack<T>::size() const
+//{
+//	return stack_size;
+//}
+//
+//template <typename T>
+//void GRPStack<T>::show() const
+//{
+//	GRPNode<T> *p(FIRST->next);
+//	for (; p != nullptr; p = p->next)
+//		cout << p->data << " ";
+//	cout << endl;
+//	return;
+//}
+//
+//template <typename T>
+//bool GRPStack<T>::empty() const
+//{
+//	return current == FIRST;
+//}
+//
+//template <typename T>
+//void GRPStack<T>::clear()
+//{
+//	while (!this->empty())
+//		this->pop();
+//	stack_size = 0;
+//	return;
+//}
+
+#ifndef GRP_STACK
+#define GRP_STACK
+template <typename T>
+GRPStack<T>::GRPStack()
+{
+	FIRST = new GRPSinNode<T>;
+	stack_size = 0;
+}
+
+template <typename T>
+void GRPStack<T>::push(T input)
+{
+	FIRST->next = new GRPSinNode<T>(input, FIRST->next);
+	++stack_size;
+	return;
+}
+
+template <typename T>
+T GRPStack<T>::pop()
+{
+	GRPSinNode<T> *p(FIRST->next);
+	T ret_data(p->data);
+	FIRST->next = p->next;
+	delete p;
+	--stack_size;
+	return ret_data;
+}
+
+template <typename T>
+size_t GRPStack<T>::size() const
+{
+	return stack_size;
+}
+
+template <typename T>
+bool GRPStack<T>::empty() const
+{
+	return !(FIRST->next);
+}
+
+template <typename T>
+void GRPStack<T>::clear()
+{
+	while (!this->empty())
+		this->pop();
+	stack_size = 0;
+	return;
+}
+
+template <typename T>
+T& GRPStack<T>::top()
+{
+	return FIRST->next->data;
+}
+
+template <typename T>
+GRPStack<T>::~GRPStack()
+{
+	for (GRPSinNode<T> *p(FIRST); p != nullptr;)
+	{
+		GRPSinNode<T> *kill = p;
+		p = p->next;
+		delete kill;
+	}
+}
+
+template <typename T>
+void GRPStack<T>::show() const
+{
+	cout << "==========" << endl << "<TOP>" << endl;
+	for (GRPSinNode<T> *p(FIRST->next); p != nullptr; p = p->next)
+		cout << p->data << endl;
+	cout << "==========" << endl;
+	return;
+}
+
+#endif
+
+
+template <typename T>
+class GRPBinTree;
+
+template <typename T>
+class GRPBinTreeNode
+{
+	friend class GRPBinTree<T>;
+protected:
+	T data;
+	GRPBinTreeNode *leftChild, *rightChild;
+	GRPBinTreeNode() :leftChild(nullptr), rightChild(nullptr) {}
+	GRPBinTreeNode(T x, GRPBinTreeNode *left = nullptr, GRPBinTreeNode *right = nullptr) :data(x), leftChild(left), rightChild(right) {}
+};
+
+//这里加入一个新的结构体，用于在栈中记录创建时候的节点，需要标注此时已经到了建立节点的左子树还是右子树<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+enum Child { LEFT, RIGHT };
+
+template <typename T>
+struct GRPBinTreeBuildNode
+{
+	GRPBinTreeNode<T> *p;
+	Child child;
+};
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+template <typename T>
+class GRPBinTree
+{
+protected:
+	GRPBinTreeNode<T> *ROOT;//作为二叉树的根节点
+	T END;//数据输入停止的标志
+	GRPBinTreeNode<T>* parent(GRPBinTreeNode<T> *sub_tree, GRPBinTreeNode<T> *current);//返回父节点的主要函数，以供私有调用
+
+	bool insert(GRPBinTreeNode<T> *&sub_tree, const T &input);//插入新元素的主要实现函数，供私有调用
+	void show_data_oneline(GRPBinTreeNode<T> *x) const { cout << x->data; }//在同一行输出所有的数据点
+
+	void show_all_postoder_oneline(GRPBinTreeNode<T> *x);
+
+	void find_the_way(GRPVector<T> &store, GRPBinTreeNode<T> *x, int i);
+public:
+	GRPBinTree() :ROOT(nullptr) {}
+	GRPBinTree(T end) :END(end), ROOT(nullptr) {}
+	GRPBinTreeNode<T>* parent(GRPBinTreeNode<T> *current) { return (ROOT == nullptr || ROOT == current) ? nullptr : parent(ROOT, current); }
+	//如果当前的节点是空或者已经是根节点，则没有父节点，否则则返回父节点
+
+	int insert(const T &input);//插入新元素
+	bool empty() { return ROOT == nullptr; }//二叉树是否为空
+
+	void show_all_postoder_oneline() { show_all_postoder_oneline(ROOT); }
+
+	void preorder_create(GRPVector<T> &input, T null_node);//前序的输入建立一个二叉树，null_node代表了空节点的表示
+
+	void find_the_way(GRPVector<T> &store) { find_the_way(store, ROOT, 1); }
+};
+
+template <typename T>
+GRPBinTreeNode<T>* GRPBinTree<T>::parent(GRPBinTreeNode<T>* sub_tree, GRPBinTreeNode<T>* current)
+{
+	//从sub_tree开始，向下寻找current的父节点
+	if (sub_tree == nullptr)
+		return nullptr;
+	else if (sub_tree->leftChild == current || sub_tree->rightChild == current)
+		return sub_tree;
+	else
+	{
+		GRPBinTreeNode<T> *p;
+		if ((p = parent(sub_tree->leftChild, current)) != nullptr)
+			return p;//递归左子树进行查找
+		else
+			return parent(sub_tree->rightChild, current);//在右子树寻找
+	}
+}
+
+template <typename T>
+void GRPBinTree<T>::show_all_postoder_oneline(GRPBinTreeNode<T>* x)
+{
+	if (x != nullptr)
+	{
+		show_all_postoder_oneline(x->leftChild);
+		show_all_postoder_oneline(x->rightChild);//通过递归进行后序遍历
+		show_data_oneline(x);
+	}
+	return;
+}
+
+template <typename T>
+void GRPBinTree<T>::preorder_create(GRPVector<T>& input, T null_node)
+{
+	if (input.size() == 0 || input[0] == null_node)
+	{
+		ROOT = nullptr;
+		return;
+	}
+	ROOT = new GRPBinTreeNode<T>;
+	ROOT->data = input[0];
+	//对于第一个节点的单独讨论
+	GRPStack<GRPBinTreeBuildNode<T>> node_stack;
+	node_stack.push({ ROOT,LEFT });
+	for (int i(1); i < input.size(); ++i)
+	{
+		if (input[i] != null_node)
+		{
+			if (node_stack.top().child == LEFT)
+			{
+				node_stack.top().p->leftChild = new GRPBinTreeNode<T>(input[i]);
+				node_stack.top().child = RIGHT;
+				node_stack.push({ node_stack.top().p->leftChild,LEFT });
+			}
+			else
+			{
+				GRPBinTreeNode<T> *p = node_stack.top().p->rightChild = new GRPBinTreeNode<T>(input[i]);
+				node_stack.pop();
+				node_stack.push({ p,LEFT });
+			}
+		}
+		else
+		{
+			if (node_stack.top().child == LEFT)
+				node_stack.top().child = RIGHT;
+			else
+				node_stack.pop();
+		}
+	}
+	return;
+}
+
+template <typename T>
+void GRPBinTree<T>::find_the_way(GRPVector<T>& store, GRPBinTreeNode<T>* x, int i)
+{
+	store[i - 1] = x->data;//将该节点放入输出准备
+	//cout << i << endl;
+	if (x->leftChild == nullptr && x->rightChild == nullptr)
+	{
+		//cout << "深度为" << i << endl;
+		//cout << "find the end" << endl;
+		for (int j(i - 1); j >= 0; --j)
+			cout << store[j];
+		cout << endl;
+		//return;
+	}
+	else
+	{
+		if (x->leftChild != nullptr)
+		{
+			//cout << x->data << "左子树为" << x->leftChild->data << endl;
+			find_the_way(store, x->leftChild, i + 1);
+		}
+		if (x->rightChild != nullptr)
+		{
+			//cout << x->data << "右子树为" << x->rightChild->data << endl;
+			find_the_way(store, x->rightChild, i + 1);
+		}
+		//return;
+	}
+	return;
+}
+
+
+#endif
+
+
+
+
+int main()
+{
+	GRPBinTree<char> tree;
+	string input;
+	cin >> input;
+	GRPVector<char> output;
+	GRPVector<char> input_resave;
+	//if (input != "ABCD@@@@@" && input!="ABD@@@CE@@F@@" && input != "ABD@@@C@E@@")
+	//cout << input << endl;
+	for (auto x : input)
+		input_resave.push_back(x);
+	for (int i(0); i < input.length(); ++i)
+		output.push_back('\0');
+	tree.preorder_create(input_resave, '@');
+	//tree.show_all_postoder_oneline();
+	tree.find_the_way(output);
+	return 0;
+}
---
+++ 广义表逆向输出.cpp
@@ -0,0 +1,487 @@
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::istream;
+using std::ostream;
+
+#define DEFAULTSIZE 5
+
+template <typename T>
+class GRPVector
+{
+protected:
+	T *data;
+	size_t max_size;
+	size_t real_size;
+public:
+	GRPVector();
+
+	void push_back(T input);
+	void pop_back();
+	T pop_head();
+	size_t size() const;
+
+	T& front();
+	T& back();
+	T& operator[](int i);
+};
+
+template <typename T>
+GRPVector<T>::GRPVector() : data(new T[DEFAULTSIZE]), max_size(5), real_size(0) {}
+
+
+template <typename T>
+size_t GRPVector<T>::size() const
+{
+	return real_size;
+}
+
+template <typename T>
+void GRPVector<T>::push_back(T input)
+{
+	if (real_size == max_size)
+	{
+		T *old = data;
+		data = new T[2 * max_size];//开一个扩容一倍的数组
+		for (size_t i(0); i < max_size; ++i)
+		{
+			data[i] = old[i];
+		}
+		max_size *= 2;//将扩容反馈
+	}
+	//无论需不需要扩容，后续的过程是一样的
+	data[real_size] = input;
+	++real_size;
+	return;
+}
+
+template <typename T>
+void GRPVector<T>::pop_back()
+{
+	//现如今没有打算完成可以自动减少长度的功能
+	--real_size;//直接将最后的标识向前即可，这样子就可以避免访问
+	return;
+}
+
+template <typename T>
+T GRPVector<T>::pop_head()
+{
+	T ret = data[0];
+	data = &data[1];
+	max_size--;
+	real_size--;
+	return ret;
+}
+
+
+template <typename T>
+T& GRPVector<T>::operator[](int i)
+{
+	//cout << -static_cast<int>(real_size) << endl;
+	//cout << (i >= -static_cast<int>(real_size)) << endl;
+	if ((i < static_cast<int>(real_size)) && (i >= -static_cast<int>(real_size)))
+	{
+		if (i >= 0)
+			return data[i];
+		else
+			return data[this->size() + i];
+	}
+
+	else
+	{
+		cout << "This GRPVector's size is " << real_size << endl;
+		cout << "You should not read the place [" << i << "]" << endl;
+		exit(-1);
+	}
+}
+
+template <typename T>
+T& GRPVector<T>::front()
+{
+	return (*this)[0];
+}
+
+template <typename T>
+T& GRPVector<T>::back()
+{
+	return (*this)[this->size()];
+}
+
+
+template <typename T>
+class GRPGenListItem;//作为返回值类型
+
+template <typename T>
+class GRPGenListNode;//作为广义表的节点类型
+
+template <typename T>
+class GRPGenList;//作为广义表的主体
+
+template <typename T>
+class GRPGenListItem
+{
+	friend class GRPGenList<T>;
+private:
+	int type;
+	union
+	{
+		int ref;
+		T data;
+		GRPGenListNode<T> *link;
+	}info;
+
+public:
+	GRPGenListItem() : type(0) { info.ref = 0; }
+	GRPGenListItem(GRPGenListItem<T> &copy) { type = copy.type; info = copy.info; }
+};
+
+template <typename T>
+class GRPGenListNode
+{
+	friend class GRPGenList<T>;
+private:
+	int type;//取值只可能是0/1/2，决定了当前节点的类型，0为头节点，1位数据，2为指针域
+	GRPGenListNode<T> *next_link;//指向同一个层级的下一个节点
+	union
+	{
+		int ref;//如果是头结点，则存储的是引用计数
+		T data;//如果是数据
+		GRPGenListNode<T> *link;
+	}info;
+
+public:
+	GRPGenListNode() :type(0), next_link(nullptr) { info.ref = 0; }//作为头结点的时候的建立
+	GRPGenListNode(GRPGenListNode<T> &copy) { type = copy.type; next_link = copy.next_link; info = copy.info; }//拷贝赋值
+	~GRPGenListNode() {}
+};
+
+template <typename T>
+class GRPGenList
+{
+private:
+	GRPGenListNode<T> *HEAD;
+	GRPGenListNode<T> *copy(GRPGenListNode<T> *x);//复制一个x指示的无共享非递归表
+	int length(GRPGenListNode<T> *x);
+	int depth(GRPGenListNode<T> *x);
+
+public:
+	GRPGenList();
+	//~GRPGenList();
+	bool head(GRPGenListItem<T> &x);//返回头元素
+	bool tail(GRPGenList<T> &it);
+	void copy(const GRPGenList<T> &x);//广义表的复制
+	GRPGenListNode<T>* first();//返回广义表的第一个元素，如果为空，则返回一个空指针nullptr
+	GRPGenListNode<T>* next(GRPGenListNode<T> *x);//返回所给元素的直接后续元素
+	int length();
+	int depth();
+	//另外需要一个建立广义表的算法
+	void createlist(GRPVector<T> &input, GRPGenListNode<T> *&x);//这里是仿照书上所做的构建函数，可能会不符合自己的预期
+	void Creat(GRPVector<T> &input);
+	void remove(GRPGenListNode<T> *it);
+	void save_to(GRPVector<T> &output, GRPGenListNode<T> *x);
+	void Save(GRPVector<T> &output);
+};
+
+template <typename T>
+GRPGenList<T>::GRPGenList()
+{
+	HEAD = new GRPGenListNode<T>;
+	if (HEAD == nullptr)
+	{
+		cout << "The HEAD is nullptr!" << endl;
+		exit(-1);
+	}
+}
+
+template <typename T>
+bool GRPGenList<T>::head(GRPGenListItem<T>& x)
+{
+	if (HEAD->next_link == nullptr)
+		return false;//此时是没有头的
+	else
+	{
+		x.type = HEAD->next_link->type;
+		x.info = HEAD->next_link->info;
+		return true;//需要返回的通过引用进行返回
+	}
+}
+
+template <typename T>
+bool GRPGenList<T>::tail(GRPGenList<T>& it)
+{
+	if (HEAD->next_link == nullptr)
+		return false;//空表，没有tail可以返回
+	else
+	{
+		it.HEAD->type = 0;//作为一个头结点
+		it.HEAD->info.ref = 0;
+		it.HEAD->next_link = copy(HEAD->next_link->next_link);//将tail的头放入头结点之后
+		return true;
+	}
+}
+
+template <typename T>
+void GRPGenList<T>::copy(const GRPGenList<T> &x)
+{
+	HEAD = copy(x.HEAD);//调用了私有函数，定义如下
+}
+
+template <typename T>
+GRPGenListNode<T>* GRPGenList<T>::copy(GRPGenListNode<T>* x)
+{
+	GRPGenListNode<T> *p = nullptr;
+	if (x != nullptr)
+	{
+		p = new GRPGenListNode<T>;
+		p->type = x->type;//将这个节点的类型设置成为拷贝进来的类型
+		switch (x->type)
+		{
+		case 0:p->info.ref = x->info.ref; break;
+		case 1:p->info.data = x->info.data; break;
+		case 2:p->info.link = x->info.link; break;
+		default:cout << x->type << " is not expected as a type!" << endl; exit(-1); break;
+		}
+		p->next_link = copy(x->next_link);//处理同一层的下一个节点
+	}
+	return p;
+}
+
+template <typename T>
+GRPGenListNode<T>* GRPGenList<T>::first()
+{
+	if (HEAD->next_link == nullptr)
+		return nullptr;
+	else
+		return HEAD->next_link;
+}
+
+template <typename T>
+GRPGenListNode<T>* GRPGenList<T>::next(GRPGenListNode<T>* x)
+{
+	if (x->next_link == nullptr)
+		return nullptr;
+	else
+		return x->next_link;
+}
+
+template <typename T>
+int GRPGenList<T>::length()
+{
+	return length(HEAD);
+}
+
+template <typename T>
+int GRPGenList<T>::length(GRPGenListNode<T>* x)
+{
+	//相当于一个递归调用，不断寻找该链条
+	if (x != nullptr)
+		return 1 + length(x->next_link);
+	else
+		return 0;
+}
+
+template <typename T>
+int GRPGenList<T>::depth()
+{
+	return depth(HEAD);
+}
+
+template <typename T>
+int GRPGenList<T>::depth(GRPGenListNode<T>* x)
+{
+	//一样的是一个递归调用
+	/*
+	 * depth有三种可能的情形
+	 * 如果x为空表：1
+	 * 如果x为原子：0
+	 * default：1+max{depth} 其中，depth为表中所有对应的子表的深度
+	 */
+	if (x->next_link == nullptr)
+		return 1;
+	GRPGenListNode<T> *p = x->next_link;
+	int m(0), n(0);//两个临时变量
+	while (p != nullptr)//空则不考虑
+	{
+		if (p->type == 2)//如果是广义表的节点，而非元素（不可能是表头）
+		{
+			n = depth(p->info.link);//计算以该节点为头的广义表的深度
+			if (m < n)//此时m已然不是最大深度
+				m = n;
+		}
+		p = p->next_link;
+	}
+	return m + 1;//因为还要算上自己的深度
+}
+
+
+template <typename T>
+void GRPGenList<T>::createlist(GRPVector<T> &input, GRPGenListNode<T> *&x)
+{
+	//cout << "!" << endl;
+	T input_data;//作为读取出来的数据
+	//在这里，希望能够直接达到DSOJ的输入要求，而非书上的
+	//DSOJ输入样例：((a,(b,c)),(),(d))
+	//如上
+	//此时的建立均没有考虑共享表
+	//input >> input_data;//读入一个单位的数据
+	if (input.size() == 0)
+		return;
+	else
+		input_data = input.pop_head();
+	//cout << input_data << endl;
+	//cout << input.size() << endl;
+	if (input_data == '(')//如果是左括号，则为表
+	{
+		x = new GRPGenListNode<T>;
+		x->type = 2;//建立了一个子表节点
+
+		x->info.link = new GRPGenListNode<T>;//新加节点
+		x->info.link->type = 0;//是个表头
+		x->info.link->info.ref = 1;//被引用了一次
+
+		//cout << "here" << endl;
+		createlist(input, x->info.link->next_link);//递归建立子表，同层向后
+		//cout << "here" << endl;
+		createlist(input, x);
+		//input_data = input.pop_head();
+	}
+
+	else if (input_data == ',')
+	{
+		//此时应该是建立后续节点
+		
+		createlist(input, x->next_link);
+	}
+
+	else if (input_data == '#')
+	{
+		//此时代表为空表
+		//cout << "?" << endl;
+		x = nullptr;
+		input.pop_head();
+		//createlist(input, x);
+	}
+
+	else if (input_data == ')')
+	{
+		//说明该层已经结束了
+		//input_data = input.pop_head();
+		x->next_link = nullptr;
+	}
+
+	else
+	{
+		//此时是元素的节点
+		x = new GRPGenListNode<T>;
+		x->type = 1;//data节点
+		x->info.data = input_data;
+		createlist(input, x);
+	}
+	return;
+}
+
+template <typename T>
+void GRPGenList<T>::Creat(GRPVector<T>& input)
+{
+	createlist(input, HEAD->next_link);
+}
+
+
+template <typename T>
+void GRPGenList<T>::remove(GRPGenListNode<T>* it)
+{
+	--(it->info.ref);//将引用减1，因为只有当引用为0的时候，才可以放心的删除这个头结点
+	if (it->info.ref == 0)//此时就可以删除了
+	{
+		GRPGenListNode<T> *p(nullptr);
+		while (it->next_link != nullptr)//当同层的下一个节点存在
+		{
+			p = it->next_link;
+			if (p->type == 2)//如果是link域
+			{
+				remove(p->info.link);//递归求，向下进行删除
+				if (p->info.link->info.ref <= 0)//所指向的头结点的引用已经安全
+					delete p->info.link;//所以删除
+			}
+			it->next_link = p->next_link;
+			delete p;
+		}
+	}
+}
+
+template <typename T>
+void GRPGenList<T>::save_to(GRPVector<T>& output, GRPGenListNode<T>* x)
+{
+	if (x == nullptr)
+		return;
+	else if (x->type == 0)//此时是头结点
+	{
+		if (x->next_link == nullptr)//此时是空
+		{
+			output.push_back('@');
+			return;
+		}
+		else
+		{
+			save_to(output, x->next_link);
+		}
+	}
+
+	else if (x->type == 1)
+	{
+		//此时是数据域
+		output.push_back(x->info.data);
+		save_to(output, x->next_link);
+	}
+
+	else
+	{
+		//此时是link域
+		save_to(output, x->info.link);
+		save_to(output, x->next_link);
+	}
+	return;
+}
+
+template <typename T>
+void GRPGenList<T>::Save(GRPVector<T>& output)
+{
+	save_to(output, HEAD);
+}
+
+
+int main()
+{
+	GRPGenList<char> x;
+	string input;
+	cin >> input;
+	GRPVector<char> input_resave;
+	for (int i(0); i < input.length(); ++i)
+	{
+		input_resave.push_back(input[i]);
+		if (input[i] == '(' && input[i + 1] == ')')
+		{
+			//cout << "?" << endl;
+			input_resave.push_back('#');
+		}
+	}
+	/*if (input_resave[0] == '(' && input_resave[-1] == ')')
+	{
+		input_resave.pop_head();
+		input_resave.pop_back();
+	}*/
+	x.Creat(input_resave);
+	GRPVector<char> output;
+	x.Save(output);
+	
+	for (int i(1); i <= output.size(); ++i)
+		cout << output[-i];
+
+	//cout << x.depth() << endl;
+	return 0;
+}
